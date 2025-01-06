b57e2a3f54272029632130ad5239774c911767aa
raza1315 <razasfs2003@gmail.com>
2025-01-07 02:08:07 +0530

    hero bg radial done

--- src/Pages/Landing/components/HeroSection.jsx
+++ src/Pages/Landing/components/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div className="relative">
       {/* radial bg absolute */}
-      {/* <div className="absolute bg-green-100 opacity-80 w-[600px] h-[210px] rounded-full blur-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-1 "></div> */}
+      <div className="absolute bg-green-100 opacity-70 w-[600px] h-[210px] rounded-full blur-3xl top-[53%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-1 "></div>
 
       {/* Hero Section */}
       <section className="py-16 px-4 z-10">
@@ -14,15 +14,15 @@ function HeroSection() {
             <br className="hidden sm:block" />
             <span className="text-[#21A26B]">loan</span> for your growth
           </h1>
-          <p className="text-[#878D97] mb-8 mt-5 max-w-xl mx-auto">
+          <p className="text-[#878D97] mb-8 mt-6 max-w-xl mx-auto">
             Certain aspects of your business might require extra financial
             support. We're here to help you grow.
           </p>
           <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
-            <button className="bg-gradient-to-r from-emerald-600 to-emerald-500 text-white px-8 py-3 rounded-full hover:opacity-90 transition-opacity w-full sm:w-auto z-10">
+            <button className="bg-gradient-to-r from-emerald-600 to-emerald-500 text-white px-8 py-3 rounded-full hover:opacity-90 transition-opacity w-full sm:w-auto z-10 cursor-pointer">
               Apply now
             </button>
-            <button className="border border-[#E2E8F0] text-gray-500 px-8 py-3 rounded-full hover:bg-gray-50 transition-colors w-full sm:w-auto z-10">
+            <button className="bg-white border border-[#E2E8F0] text-gray-500 px-8 py-3 rounded-full hover:bg-gray-50 transition-colors w-full sm:w-auto z-10 cursor-pointer">
               Learn More
             </button>
           </div>

52250f7f6c4795e8ae652cf08e884fc5b86d6a38
raza1315 <razasfs2003@gmail.com>
2025-01-07 04:23:16 +0530

    done

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -10,7 +10,7 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="bg-emerald-900 text-gray-300">
+    <footer className="bg-[#074E3B] text-gray-300">
       <div className="container mx-auto px-4 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Company Info */}
